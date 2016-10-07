9de2eb467952d62a45bfd50de4f7dc74668823d6
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-10-07 10:20:49 -0400

    Add new apis

--- src/data-structures/Graph.js
+++ src/data-structures/Graph.js
@@ -9,31 +9,35 @@ export default class Graph {
     this.nodes = {}
     this.adj = {}
     this.edge = this.adj
-    this.nodeSize = 0
-    this.edgeSize = 0
   }
 
-  add_node(id) {
+  add_node(id,attr) {
     if(!this.nodes[id]) {
-      this.nodeSize++
-      this.nodes[id] = {}
+      this.nodes[id] = attr ? attr : {}
       this.adj[id] = {}
+    } else {
+      this.nodes[id] = _.assignIn(this.nodes[id],attr)
     }
   }
 
-  add_nodes_from(ids) {
+  add_nodes_from(ids,attr) {
     ids.forEach((id) => {
-      this.add_node(id)
+      this.add_node(id,attr)
     })
   }
 
   remove_node(id) {
-    let node_to_remove = this.nodes[id]
-    if(node_to_remove) {
-      //
-    } else {
-      return
+    let adj = self.adj
+    let nbrs = []
+    if(this.nodes[id]) {
+      nbrs = _.keys(adj[id])
+      delete this.nodes[id]
     }
+    nbrs.forEach((u)=>{
+      delete adj[u][id]
+    })
+    delete adj[id]
+    this.adj = adj
   }
 
   get_node(id) {
@@ -46,6 +50,71 @@ export default class Graph {
 
   has_node(id) {
     return _.hasIn(this.nodes,id + '')
+  }
+
+  num_nodes() {
+    return _.size(this.nodes)
+  }
+
+  /**
+   * Only supports single attribute for now
+   */
+  add_edge(u,v,attr) {
+    if(!this.nodes[u]) {
+      this.adj[u] = {}
+      this.nodes[u] = {}
+    }
+    if(!this.nodes[v]) {
+      this.adj[v] = {}
+      this.nodes[v] = {}
+    }
+    let data = this.adj[u][v] ? this.adj[u][v] : {}
+    this.adj[u][v] = _.assignIn(data,attr)
+    this.adj[v][u] = _.assignIn(data,attr)
+  }
+
+  add_edges_from(edgeb,attr) {
+    attr = attr ? attr : {}
+    edgeb.forEach((e)=> {
+      let u,v,d
+      if(e.length == 3) {
+          u = e[0]
+          v = e[1]
+          d = e[2]
+      } else if(e.length == 2) {
+        u = e[0]
+        v = e[1]
+        d = {}
+      } else {
+        console.log('error')
+      }
+      if(!this.nodes[u]) {
+        this.adj[u] = {}
+        this.nodes[u] = {}
+      }
+      if(!this.nodes[v]) {
+        this.adj[v] = {}
+        this.nodes[v] = {}
+      }
+      let data = this.adj[u][v] ? this.adj[u][v] : {}
+      data = _.assignIn(data,attr)
+      data = _.assignIn(data,d)
+      this.adj[u][v] = data
+      this.adj[v][u] = data
+    })
+  }
+
+  add_weighted_edges_from(edgeb,weight='weight',...attr) {
+    edgeb.forEach((e)=> {
+      let u = e[0]
+      let v = e[1]
+      let d = e[2]
+      this.add_edges_from([[u,v,{weight:d}]],attr)
+    })
+  }
+
+  remove_edge() {
+
   }
 
 }
--- test/data-structures/GraphTest.js
+++ test/data-structures/GraphTest.js
@@ -16,13 +16,22 @@ describe('Graph Test Suite', ()=> {
   it('should be able to add node', ()=> {
     G.add_node(1)
     G.add_node(3)
-    expect(G.nodeSize).to.equal(2)
+    expect(_.size(G.nodes)).to.equal(2)
     expect(_.keys(G.nodes)).to.deep.equal(['1','3'])
+  })
+
+  it('should be able to add node as well as property',()=> {
+    G.add_node(1,{'weight': 2})
+    G.add_node(2,{'weight': 5})
+    expect(_.size(G.nodes)).to.equal(2)
+    expect(_.keys(G.nodes)).to.deep.equal(['1','2'])
+    expect(_.keys(G.nodes['1'])).to.deep.equal(['weight'])
+    expect(G.nodes['1'].weight).to.equal(2)
   })
 
   it('should be able to add multiple nodes', ()=> {
     G.add_nodes_from([1,2,3])
-    expect(G.nodeSize).to.equal(3)
+    expect(_.size(G.nodes)).to.equal(3)
     expect(_.keys(G.nodes)).to.deep.equal(['1','2','3'])
   })
 
@@ -36,5 +45,26 @@ describe('Graph Test Suite', ()=> {
     G.add_nodes_from([1,2])
     expect(G.has_node(2)).to.be.true
     expect(G.has_node(4)).to.be.false
+  })
+
+  it('should be able to add edges',() => {
+    G.add_edge(1,2)
+    G.add_edge(4,5,{'weight' : 3})
+    expect(G.adj[1][2]).to.be.eql({})
+    expect(G.adj[4][5]).to.be.eql({'weight' : 3})
+  })
+
+  it('should be able to add edges from tuples', ()=>{
+    G.add_edges_from([[1,2],[2,3]],{'weight' : 2})
+    expect(G.adj[1][2]).to.be.eql({'weight' : 2})
+    expect(G.adj[2][3]).to.be.eql({'weight' : 2})
+    G.add_edges_from([[3,4,{'weight' : 4}]])
+    expect(G.adj[3][4]).to.be.eql({'weight' : 4})
+  })
+
+  it('should be able to add weighted edges', ()=>{
+    G.add_weighted_edges_from([[1,2,0.4],[2,3,0.7]])
+    expect(G.adj[1][2]).to.be.eql({'weight':0.4})
+    expect(G.adj[2][3]).to.be.eql({'weight':0.7})
   })
 })
